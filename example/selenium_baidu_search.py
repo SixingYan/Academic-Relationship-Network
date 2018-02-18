ef2b869bd5c70bc789c314b393b8b1c9aea7c975
Yan, Sixing <plutoyem@outlook.com>
2018-02-18 08:34:37 +0800

    new

--- example/selenium_baidu_search.py
+++ example/selenium_baidu_search.py
@@ -3,13 +3,15 @@ TASK: use Selenium to search keywork 'selenium' in baidu.com, then return the we
 """
 #====== import
 from selenium import webdriver
-from time import sleep
+#from time import sleep
 
 def main():
     # 后面是你的浏览器驱动位置，记得前面加r'','r'是防止字符转义的
-    driver = webdriver.Chrome(r'C:\Python34\chromedriver_x64.exe')
+    #driver = webdriver.Chrome(r'C:\Python34\chromedriver_x64.exe')
+    driver = webdriver.Chrome()
     # 用get打开百度页面
     driver.get("http://www.baidu.com")
+    '''
     # 查找页面的“设置”选项，并进行点击
     driver.find_elements_by_link_text('设置')[0].click()
     # 打开设置后找到“搜索设置”选项，设置为每页显示50条
@@ -32,6 +34,6 @@ def main():
     sleep(2)
     # 在打开的页面中找到“Selenium - 开源中国社区”，并打开这个页面
     driver.find_elements_by_link_text('Selenium - 开源中国社区')[0].click()
-
+    '''
 if __name__ == '__main__':
     main()
---
+++ example/selenium_chromedriver.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+brower = webdriver.Chrome()
+
+try:
+    brower.get('http://www.baidu.com')
+    #widget_playcount = brower.find_element_by_id("widget-playcount")
+    #print(widget_playcount.text)
+    print(brower.current_url)
+finally:
+    brower.close()
